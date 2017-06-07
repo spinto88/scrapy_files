9f754f38f930f2afc0507eed83076f93056ba57a
Seba <seba-pinto@hotmail.com>
2017-06-07 19:10:45 -0300

    La Nacion ediciones anteriores y creador de database agregado

---
+++ database.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sqlite3
+import os
+import datetime
+from datetime import timedelta
+from lxml import etree
+
+table_to_save = 'lanacion'
+file_of_data = 'lanacion.xml'
+description_of_the_text = 'Notas del diario La Nación, tomadas de la página web indexadas como http://www.lanacion.com.ar/id, donde id es el número de nota. Las notas publicadas en la edición impresa tienen el atributo time = 00:00:00'
+
+data = etree.parse(file_of_data)
+
+con = sqlite3.connect('data.db')
+cursor = con.cursor()
+
+try:
+    cursor.execute("create table {} \n /* {} */ \n(id int primary key, \ndate date /* Fecha */, \ntime time /* Hora de publicación */, \nprefix text /* Volanta */, \ntitle text /* Título de la nota */, \nsubtitle text /* Subtítulo o copete */, \nsection text /* Sección */, \nauthor text /* Autor de la nota */, \nnewspaper text /* Diario o portal de noticias */, \nbody text /* Cuerpo de la nota */, \ntag text /* Etiqueta propuesta por el periódico */, \nurl text /* Página web */);".format(table_to_save, description_of_the_text))
+except:
+    pass
+
+try: 
+    cursor.execute("select MAX(id) from {};".format(table_to_save))
+    for i in cursor:
+        id_note = i[0] + 1
+except:
+    id_note = 0
+
+items = data.xpath('//item')
+for item in items:
+
+    order = 'insert into {} (id) values ({});'.format(table_to_save, id_note)
+    cursor.execute(order)
+
+    try:
+        title = item.xpath('title/text()')[0]
+        title = title.replace('"','\'')
+        order = 'update {} set title  = "{}" where id = {};'\
+              .format(table_to_save, title.encode("utf-8"), id_note)
+        cursor.execute(order)
+    except: 
+        pass
+
+    try:
+        prefix = item.xpath('prefix/text()')[0]
+        prefix = prefix.replace('"','\'')
+        order = 'update {} set prefix  = "{}" where id = {};'\
+              .format(table_to_save, prefix.encode("utf-8"), id_note)
+        cursor.execute(order)
+    except: 
+        pass
+
+    try:
+        subtitle = item.xpath('subtitle/text()')[0]
+        subtitle = subtitle.replace('"','\'')
+        order = 'update {} set subtitle  = "{}" where id = {};'\
+                 .format(table_to_save, subtitle.encode("utf-8"), id_note)
+        cursor.execute(order)
+    except: 
+        pass
+
+    try:
+        body = item.xpath('body/text()')[0]
+        body = body.replace('"','\'')
+        order = 'update {} set body  = "{}" where id = {};'\
+                 .format(table_to_save, body.encode("utf-8"), id_note)
+        cursor.execute(order)
+    except: 
+        pass
+
+    try:
+        section = item.xpath('section/text()')[0]
+        section = section.replace('"','\'')
+        order = 'update {} set section  = "{}" where id = {};'\
+                 .format(table_to_save, section.encode("utf-8"), id_note)
+        cursor.execute(order)
+    except: 
+        pass
+
+    try:
+        author = item.xpath('author/text()')[0]
+        author = author.replace('"','\'')
+        order = 'update {} set author  = "{}" where id = {};'\
+                 .format(table_to_save, author.encode("utf-8"), id_note)
+        cursor.execute(order)
+    except: 
+        pass
+
+    try:
+        newspaper = item.xpath('newspaper/text()')[0]
+        newspaper = newspaper.replace('"','\'')
+        order = 'update {} set newspaper  = "{}" where id = {};'\
+                 .format(table_to_save, newspaper.encode("utf-8"), id_note)
+        cursor.execute(order)
+    except: 
+        pass
+
+    try:
+        url = item.xpath('url/text()')[0]
+        url = url.replace('"','\'')
+        order = 'update {} set url  = "{}" where id = {};'\
+                 .format(table_to_save, url.encode("utf-8"), id_note)
+        cursor.execute(order)
+    except: 
+        pass
+
+    try:
+        tag = item.xpath('tag/text()')[0]
+        tag = tag.replace('"','\'')
+        order = 'update {} set tag  = "{}" where id = {};'\
+                 .format(table_to_save, tag.encode("utf-8"), id_note)
+        cursor.execute(order)
+    except: 
+        pass
+
+    try:
+        date = item.xpath('date/text()')[0]
+        order = 'update {} set date  = "{}" where id = {};'\
+                 .format(table_to_save, date, id_note)
+        cursor.execute(order)
+    except: 
+        pass
+
+    try:
+        time = item.xpath('time/text()')[0]
+        order = 'update {} set time  = "{}" where id = {};'\
+                 .format(table_to_save, time, id_note)
+        cursor.execute(order)
+    except: 
+        pass
+    
+    con.commit()
+    id_note += 1
+    
+con.close()
---
+++ lanacion_ed_anteriores.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+
+class Item(scrapy.Item):
+    title = scrapy.Field()
+    subtitle = scrapy.Field()
+    body = scrapy.Field()
+    date = scrapy.Field()
+    time = scrapy.Field()
+    newspaper = scrapy.Field()
+    section = scrapy.Field()
+    tag = scrapy.Field()
+    author = scrapy.Field()
+    url = scrapy.Field()
+
+class LaNacionSpider(scrapy.Spider):
+    name = "lanacion"
+
+    def start_requests(self):
+        urls = []
+        for i in range(2030000, 2030200):
+            urls.append('http://www.lanacion.com.ar/' + str(i))
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+
+        url = response.url
+
+        try:
+            title = response.selector.xpath('//*[@itemprop = "headline"]/text()')[0].extract()
+        except:
+            title = ''
+
+        try:
+            subtitle = response.selector.xpath('//*[@itemprop = "description"]/text()')[0].extract()
+        except:
+            subtitle = ''
+
+        try:
+            body = response.selector.xpath('//*[@itemprop = "articleBody"]//*/text()')
+        except:
+            body = ''
+
+        try:
+            date_time = response.selector.xpath('//div[@class = "fecha"]//@content')[0].extract()
+            date = date_time.split(' ')[0] 
+            time = date_time.split(' ')[1]
+        except:
+            date = ''
+            time = ''
+
+        try:
+            names = response.selector.xpath('//span[@itemprop = "name"]/text()')
+        except:
+            names = ''
+
+        try:
+            author = response.selector.xpath('//a[@itemprop = "author"]/text()')[0].extract()
+        except:
+            author = ''
+
+        item = Item()
+        item['title'] = title
+        item['subtitle'] = subtitle
+        item['date'] = date
+        item['time'] = time
+        item['author'] = author
+        item['url'] = url
+
+        try:
+            item['newspaper'] = names[0].extract()
+        except:
+            item['newspaper'] = u'LaNación'
+
+        try:
+            item['section'] = names[1].extract()
+        except:
+            item['section'] = ''
+
+        try:
+            item['tag'] = names[2].extract()
+        except:
+            item['tag'] = ''
+
+        body_text = ''
+        try:
+            for text in body:
+                body_text += text.extract() + ' '
+        except: 
+            pass
+
+        item['body'] = body_text
+
+        return item 
