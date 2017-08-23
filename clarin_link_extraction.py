04e3bcd41f0effac15c4d8eb5b6661abd26b62d7
Seba <seba-pinto@hotmail.com>
2017-08-23 20:07:42 -0300

    Algunas correcciones

--- clarin_link_extraction.py
+++ clarin_link_extraction.py
@@ -8,7 +8,7 @@ sess = dryscrape.Session()
 
 sections = ['politica/', 'mundo/', 'sociedad/', 'economia/', 'policiales/']
 
-first_date = "2017-7-1"
+first_date = "2017-8-1"
 first_date = datetime.datetime.strptime(first_date, "%Y-%m-%d").date()
         
 for section in sections:
@@ -16,27 +16,27 @@ for section in sections:
     sess.visit('https://www.clarin.com/{}'.format(section))
 
     date_min = deepcopy(first_date)
-    while date_min >= first_date:
+    for i in range(1000):
 
-        sess.exec_script('window.scrollTo(0, document.body.scrollHeight);')
-        time.sleep(1)
-        
-        dates_info = sess.xpath('//div[@class = "box-notas"]//article//*[@class = "fecha"]')
+      sess.exec_script('window.scrollTo(0, document.body.scrollHeight);')
+      time.sleep(1)
 
-        dates_aux = [[daux.split('.') for daux in d.text().split(' ')] \
-              for d in dates_info] 
+    dates_info = sess.xpath('//div[@class = "box-notas"]//article//*[@class = "fecha"]')
 
-        dates = []
-        for d in dates_aux:
-          try:
+    dates_aux = [[daux.split('.') for daux in d.text().split(' ')] \
+                                      for d in dates_info] 
+    dates = []
+    for d in dates_aux:
+        try:
             dates.append(datetime.datetime.strptime('-'.join(d[1]), '%d-%m-%Y').date())
-          except:
+        except:
             try:
               dates.append(datetime.datetime.strptime('-'.join(d[0]), '%d-%m-%Y').date())
             except:
               dates.append(datetime.datetime.today().date())
 
-        date_min = min(set(dates))
+    date_min = min(set(dates))
+    print date_min
 
     links = sess.xpath('//div[@class = "box-notas"]//article//*[@href]')
     
