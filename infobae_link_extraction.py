fa9b53d8c14cf008f19bef9709fe7983c3535173
Sebastian Pinto <spinto@coimbra.df.uba.ar>
2017-09-06 15:05:10 -0300

    Infobae link extraction

--- infobae_link_extraction.py
+++ infobae_link_extraction.py
@@ -4,7 +4,7 @@ import cPickle as pk
 import datetime
 from copy import deepcopy
 
-#dryscrape.start_xvfb()
+dryscrape.start_xvfb()
 sess = dryscrape.Session()
 
 sections = ['politica', 'deportes', 'sociedad', 'economia']
@@ -13,7 +13,7 @@ for section in sections:
 
     sess.visit('https://www.infobae.com/{}/'.format(section))
 
-    for i in range(250):
+    for i in range(100):
 
      try:
       button = sess.xpath('//div[@class="button pb-loadmore clear generic-results-list-load-more"]')[0]
